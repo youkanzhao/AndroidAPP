db14cdd4d5a25439fdd1bee926c49176f0c3423f
youkanzhao <youkanzhao@gmail.com>
2013-08-08 23:00:57 +0800

    fix bugs

--- AndroidApp/src/com/jyou/android/MainActivity.java
+++ AndroidApp/src/com/jyou/android/MainActivity.java
@@ -11,8 +11,8 @@ public class MainActivity extends DroidGap {
     {  
         super.onCreate(savedInstanceState);  
         // Set by <content src="index.html" /> in config.xml  
-        super.loadUrl(Config.getStartUrl());  
-        //super.loadUrl("file:///android_asset/www/index.html")  
+//        super.loadUrl(Config.getStartUrl());  
+        super.loadUrl("file:///android_asset/www/index.html"); 
     }
 
 }
